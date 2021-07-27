29c1fd7b4dec4c705e35932dcb9a7154cd283d8d
Dominik <git@secnd.me>
2021-07-27 21:34:21 +0200

    Reduce poll interval for DNSK-P11

--- esppac_wlan.h
+++ esppac_wlan.h
@@ -12,7 +12,7 @@ namespace ESPPAC
     static const int INIT_TIMEOUT = 10000; // Time to wait before initializing after boot
     static const int INIT_END_TIMEOUT = 10000; // Time to wait for last handshake packet
     static const int FIRST_POLL_TIMEOUT = 650; // Time to wait before requesting the first poll
-    static const int POLL_INTERVAL = 55000; // The interval at which to poll the AC
+    static const int POLL_INTERVAL = 30000; // The interval at which to poll the AC
     static const int RESPONSE_TIMEOUT = 600; // The timeout after which we expect a response to our last command
     static const int INIT_FAIL_TIMEOUT = 30000; // The timeout after which the initialization is considered failed
 
