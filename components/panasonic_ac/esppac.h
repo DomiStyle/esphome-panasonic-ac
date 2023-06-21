a55ed6616d56437e2f3a367fb4719e2ff77ad4f6
Dominik <git@secnd.me>
2023-06-21 23:16:08 +0200

    Bump version

--- components/panasonic_ac/esppac.h
+++ components/panasonic_ac/esppac.h
@@ -11,7 +11,7 @@ namespace esphome {
 
 namespace panasonic_ac {
 
-static const char *const VERSION = "2.3.0";
+static const char *const VERSION = "2.4.0";
 
 static const uint8_t BUFFER_SIZE = 128;  // The maximum size of a single packet (both receive and transmit)
 static const uint8_t READ_TIMEOUT = 20;  // The maximum time to wait before considering a packet complete
