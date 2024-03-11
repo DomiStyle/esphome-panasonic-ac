6389bc94d2739128905fba276363ed5d6b050718
Steve Saunders <steve@map59.com>
2024-03-11 20:44:02 +1100

    Update esppac.cpp

--- components/panasonic_ac/esppac.cpp
+++ components/panasonic_ac/esppac.cpp
@@ -43,7 +43,7 @@ void PanasonicAC::setup() {
 void PanasonicAC::loop() {
   read_data();  // Read data from UART (if there is any)
 
-  ESPTime time = esphome::time::now()
+  ESPTime time = now()
 
   ESP_LOGD(TAG, 'Time: %d', time.second);
 }
