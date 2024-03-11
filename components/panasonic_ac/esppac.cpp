377479f9aee893818e0a4bff1d70c03ec1d5daa3
Steve Saunders <steve@map59.com>
2024-03-11 20:50:27 +1100

    Update esppac.cpp

--- components/panasonic_ac/esppac.cpp
+++ components/panasonic_ac/esppac.cpp
@@ -1,6 +1,6 @@
 #include "esppac.h"
 #include "esphome/core/log.h"
-#include "esphome/core/time.h"
+#include "esphome/components/time/real_time_clock.h"
 
 namespace esphome {
 namespace panasonic_ac {
@@ -43,7 +43,7 @@ void PanasonicAC::setup() {
 void PanasonicAC::loop() {
   read_data();  // Read data from UART (if there is any)
 
-  ESPTime time = now()
+  auto now = time::RealTimeClock::utcnow();
 
   ESP_LOGD(TAG, 'Time: %d', time.second);
 }
