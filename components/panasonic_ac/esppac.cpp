89da922f802047f9b6a6e8689e60c8c904ed9ac0
Steve Saunders <steve@map59.com>
2024-03-11 21:47:27 +1100

    Update esppac.cpp

--- components/panasonic_ac/esppac.cpp
+++ components/panasonic_ac/esppac.cpp
@@ -1,7 +1,6 @@
 #include "esppac.h"
 #include "esphome/core/log.h"
-#include "esphome/core/time.h"
-#include "time.h"
+#include "ctime.h"
 
 namespace esphome {
 namespace panasonic_ac {
@@ -41,22 +40,29 @@ void PanasonicAC::setup() {
   ESP_LOGI(TAG, "Panasonic AC component v%s starting...", VERSION);
 }
 
-time_t day_seconds() {
-    time_t t1, t2;
-    struct tm tms;
-    time(&t1);
-    localtime_r(&t1, &tms);
-    tms.tm_hour = 0;
-    tms.tm_min = 0;
-    tms.tm_sec = 0;
-    t2 = mktime(&tms);
-    return t1 - t2;
+double day_seconds() {
+  time_t now;
+  if (time(&now) == -1) {
+    return -1;
+  }
+  struct tm timestamp;
+  if (localtime_r(&now, &timestamp) == 0) { // C23
+    return -1;
+  }
+  timestamp.tm_isdst = -1; // Important
+  timestamp.tm_hour = 0;
+  timestamp.tm_min = 0;
+  timestamp.tm_sec = 0;
+  time_t midnight = mktime(&timestamp);
+  if (midnight == -1) {
+    return -1;
+  }
+  return difftime(now, midnight);
 }
 
 void PanasonicAC::loop() {
   read_data();  // Read data from UART (if there is any)
 
-  auto time = ESPTime();
   ESP_LOGD(TAG, 'Time: %s', day_seconds());
 }
 
